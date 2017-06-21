78face74b16bfc504162aaf511fae4a778d8a9f3
tianxuewei1016 <93226539@qq.com>
2017-06-21 16:16:26 +0800

    http封装

--- app/src/main/java/com/p2p0224/fragment/HomeFragment.java
+++ app/src/main/java/com/p2p0224/fragment/HomeFragment.java
@@ -1,22 +1,16 @@
 package com.p2p0224.fragment;
 
 import android.content.Context;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.alibaba.fastjson.JSON;
 import com.p2p0224.R;
+import com.p2p0224.base.BaseFragment;
 import com.p2p0224.bean.IndexBean;
 import com.p2p0224.common.AppNetConfig;
-import com.p2p0224.utils.UIUtils;
+import com.p2p0224.utils.HttpUtils;
 import com.squareup.picasso.Picasso;
 import com.youth.banner.Banner;
 import com.youth.banner.loader.ImageLoader;
@@ -37,7 +31,7 @@ import butterknife.ButterKnife;
  * 作用：
  */
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends BaseFragment {
 
     @Bind(R.id.base_title)
     TextView baseTitle;
@@ -52,59 +46,54 @@ public class HomeFragment extends Fragment {
     @Bind(R.id.tv_home_yearrate)
     TextView tvHomeYearrate;
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = UIUtils.inflate(R.layout.fragment_home);
-        ButterKnife.bind(this, view);
-        return view;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        initView();
-        initData();
-        initListener();
-    }
-
-    private void initListener() {
-
+    protected void initTitle() {
+        baseTitle.setText("首页");
     }
 
     private List<String> list = new ArrayList<>();
 
-    private void initData() {
+    public void initData() {
         loadNet();
-        initBanner();
     }
 
     /**
      * 加载网络
      */
     private void loadNet() {
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(AppNetConfig.INDEX, new AsyncHttpResponseHandler() {
+//        AsyncHttpClient client = new AsyncHttpClient();
+//        client.get(AppNetConfig.INDEX, new AsyncHttpResponseHandler() {
+//            @Override
+//            public void onSuccess(int statusCode, String content) {
+//                super.onSuccess(statusCode, content);
+////                Log.e("TAG", "onSuccess" + content);
+//                //解析数据
+////                IndexBean indexBean = JSON.parseObject(content, IndexBean.class);
+//
+//
+//                try {
+//                    parseJson(content);
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Throwable error, String content) {
+//                super.onFailure(error, content);
+//                Log.e("TAG", "onFailure" + content);
+//            }
+//        });
+        HttpUtils.getInstance().get(AppNetConfig.INDEX, new HttpUtils.OnHttpClientListener() {
             @Override
-            public void onSuccess(int statusCode, String content) {
-                super.onSuccess(statusCode, content);
-//                Log.e("TAG", "onSuccess" + content);
-                //解析数据
-//                IndexBean indexBean = JSON.parseObject(content, IndexBean.class);
-
-
-                try {
-                    parseJson(content);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+            public void onSuccess(String json) {
+                IndexBean indexBean = JSON.parseObject(json, IndexBean.class);
+                initBanner(indexBean);
             }
 
             @Override
-            public void onFailure(Throwable error, String content) {
-                super.onFailure(error, content);
-                Log.e("TAG", "onFailure" + content);
+            public void onFailure(String message) {
+
             }
         });
     }
@@ -149,9 +138,15 @@ public class HomeFragment extends Fragment {
 
     /**
      * 初始化banner
+     *
+     * @param indexBean
      */
-    private void initBanner() {
-        list.add(AppNetConfig.BASE_URL + "images/index02.png");
+    private void initBanner(IndexBean indexBean) {
+        List<IndexBean.ImageArrBean> imageArr = indexBean.getImageArr();
+        for (int i = 0; i < imageArr.size(); i++) {
+            String imaurl = imageArr.get(i).getIMAURL();
+            list.add(AppNetConfig.BASE_URL + imaurl);
+        }
         //设置图片加载器
         banner.setImageLoader(new GlideImageLoader());
         //设置图片集合
@@ -159,6 +154,7 @@ public class HomeFragment extends Fragment {
         //banner设置方法全部调用完毕时最后调用
         banner.start();
     }
+
 
     public class GlideImageLoader extends ImageLoader {
         @Override
@@ -170,6 +166,11 @@ public class HomeFragment extends Fragment {
 
     private void initView() {
 
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_home;
     }
 
     @Override
--- app/src/main/java/com/p2p0224/utils/HttpUtils.java
+++ app/src/main/java/com/p2p0224/utils/HttpUtils.java
@@ -1,19 +1,20 @@
 package com.p2p0224.utils;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 /**
  * 作者：田学伟 on 2017/6/21 14:37
  * QQ：93226539
- * 作用：
+ * 作用：网络请求的封装类
  */
 
 public class HttpUtils {
 
-    private AsyncHttpClient client;
+    private AsyncHttpClient httpClient;
 
     private HttpUtils() {
-        client = new AsyncHttpClient();
+        httpClient = new AsyncHttpClient();
     }
 
     private static HttpUtils httpUtils = new HttpUtils();
@@ -22,4 +23,52 @@ public class HttpUtils {
         return httpUtils;
     }
 
+    public void get(String url, final OnHttpClientListener onHttpClientListener) {
+        httpClient.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, String content) {
+                super.onSuccess(statusCode, content);
+                if (onHttpClientListener != null) {
+                    onHttpClientListener.onSuccess(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable error, String content) {
+                super.onFailure(error, content);
+                if (onHttpClientListener != null) {
+                    onHttpClientListener.onFailure(content);
+                }
+            }
+        });
+    }
+
+//    public void post(String url, final OnHttpClientListener onHttpClientListener){
+//        httpClient.post(url,new AsyncHttpResponseHandler(){
+//            @Override
+//            public void onSuccess(int statusCode, String content) {
+//                super.onSuccess(statusCode, content);
+//                if(onHttpClientListener!=null) {
+//                    onHttpClientListener.onSuccess(content);
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Throwable error, String content) {
+//                super.onFailure(error, content);
+//                if(onHttpClientListener!=null) {
+//                    onHttpClientListener.onFailure(content);
+//                }
+//            }
+//        });
+//    }
+
+    /**
+     * 设置接口
+     */
+    public interface OnHttpClientListener {
+        void onSuccess(String json);
+
+        void onFailure(String message);
+    }
 }
